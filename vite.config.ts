4ac146416ff2c073898f3b9588e80577dce9e1d1
Quentin Widlocher <quentin.widlocher@erudo.fr>
2021-11-02 15:39:53 +0100

    🔧 Add PWA mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'card-bg.jpg'],
+      manifest: {
+        name: 'D&D Spell Manager',
+        short_name: 'Spell Manager',
+        description: 'Manage your spells',
+        lang: 'en-US',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#3B82F6',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '152x152',
+          },
+        ],
+      },
+    }),
+  ],
 })
